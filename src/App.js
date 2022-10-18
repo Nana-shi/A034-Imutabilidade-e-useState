e0f925608c89c75794dc5886a827a6172b5499bb
Nana-shi <leof.pereira2@gmail.com>
2022-10-18 11:40:34 -0300

    Atividades aula a034-imutabilidade-e-usestate 17/10

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, {useState} from 'react'
-import styled, {createGlobalStyle} from 'styled-components'
-import PokemonCard from './Components/PokemonCard/PokemonCard';
+import React, { useState } from "react";
+import styled, { createGlobalStyle } from "styled-components";
+import PokemonCard from "./Components/PokemonCard/PokemonCard";
 
 const GlobalStyles = createGlobalStyle`
   * {
@@ -8,33 +8,45 @@ const GlobalStyles = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
   }
-`
+`;
 const FlexContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
-`
+`;
 function App() {
   const [pokemon, setPokemon] = useState({
     name: "Pichu",
     type: "Electric",
     evolved: false,
     weight: 2,
-    color: 'yellow',
-    image: 'https://archives.bulbagarden.net/media/upload/thumb/b/b9/172Pichu.png/250px-172Pichu.png',
+    color: "yellow",
+    image:
+      "https://archives.bulbagarden.net/media/upload/thumb/b/b9/172Pichu.png/250px-172Pichu.png",
     id: 0
-  })
+  });
+  const [pokemon1, setPokemon1] = useState({
+    name: "Growlithe",
+    type: "Fire",
+    evolved: false,
+    weight: 19,
+    color: "red",
+    image:
+      "https://archives.bulbagarden.net/media/upload/thumb/3/3d/058Growlithe.png/250px-058Growlithe.png",
+    id: 58
+  });
 
   // Para fazer seus próximos pokemons, crie novos estados!
 
-  return ( <>
-  <GlobalStyles/>
-    <FlexContainer>
-      {/* Aqui neste componente, passe as props. Lembre-se que também é possivel passar a função de setState via props! */}
-      <PokemonCard/>
-      {/* Crie aqui seus próximos pokemons! */}
-    </FlexContainer>
-  </>
-    
+  return (
+    <>
+      <GlobalStyles />
+      <FlexContainer>
+        {/* Aqui neste componente, passe as props. Lembre-se que também é possivel passar a função de setState via props! */}
+        <PokemonCard pokemon={pokemon} setPokemon={setPokemon} />
+        <PokemonCard pokemon={pokemon1} setPokemon={setPokemon1} />
+        {/* Crie aqui seus próximos pokemons! */}
+      </FlexContainer>
+    </>
   );
 }
 
